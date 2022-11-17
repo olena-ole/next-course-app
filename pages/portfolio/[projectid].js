1ca1946e7c6b564df05029281630190c2cad4922
Olena <pampam32121@gmail.com>
2022-11-18 01:47:29 +0200

    added dynamic route to portfolio projects

---
+++ pages/portfolio/[projectid].js
@@ -0,0 +1,15 @@
+import { useRouter } from 'next/router'
+
+
+export default function PortfolioProjectPage() {
+
+    const router = useRouter()
+    console.log(typeof router.pathname)
+    console.log(router.query)
+
+    return (
+        <div>
+            <h1>The Portfolio Project Page</h1>
+        </div>
+    )
+}
