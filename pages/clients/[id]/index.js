8d5a4bb6bc9349c2714437a470bc8032b79d5968
Olena <pampam32121@gmail.com>
2022-11-18 03:26:39 +0200

    added programmatic navigtion after clicking the btn on /clients/max projects page

--- pages/clients/[id]/index.js
+++ pages/clients/[id]/index.js
@@ -1,7 +1,20 @@
-export default function ClientProjectssPage() {
+import { useRouter } from 'next/router'
+
+export default function ClientProjectsPage() {
+
+    const router = useRouter()
+
+    function loadProjectHandler() {
+        router.push({
+            pathname: '/clients/[id]/[projectid]',
+            query: {id: 'max', projectid: 'projecta'}
+        })
+    }
+
     return (
         <div>
             <h1>The Projects of a Given Client</h1>
+            <button onClick={loadProjectHandler}>Load project A</button>
         </div>
     )
 }
