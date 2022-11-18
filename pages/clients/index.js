8680a63dbcd3dcbfdd5c6c3177cc7e67823b9ab7
Olena <pampam32121@gmail.com>
2022-11-18 03:08:01 +0200

    added navigation to clients dynmic routes

--- pages/clients/index.js
+++ pages/clients/index.js
@@ -1,7 +1,24 @@
+import Link from 'next/link'
+
 export default function ClientsPage() {
+
+    const clients = [
+        {id: 'max', name: 'Maximilian'},
+        {id: 'manu', name: 'Manual'}
+    ]
+
     return (
         <div>
             <h1>Clients Page</h1>
+            <ul>
+                {clients.map(client => (
+                    <li key={client.id}>
+                        <Link href={`/clients/${client.id}`}>
+                            {client.name}
+                        </Link>
+                    </li>
+                ))}
+            </ul>
         </div>
     )
 }
